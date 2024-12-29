c4ec92be19216f74c11e8cbc1a2d1b0f43817254
Sebastian <sebastian.soto1649@gmail.com>
2024-12-29 14:21:30 -0400

    Auto commit

--- choco_hypewear/settings.py
+++ choco_hypewear/settings.py
@@ -108,9 +108,25 @@ SHIPPING_BOXES = [
 
 import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
-}
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+if DEBUG:
+    # Local development database
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DB_NAME', default='myprojectdb'),
+            'USER': config('DB_USER', default='soto2571'),
+            'PASSWORD': config('DB_PASSWORD', default='123456789'),
+            'HOST': config('DB_HOST', default='localhost'),
+            'PORT': config('DB_PORT', default='5432'),
+        }
+    }
+else:
+    # Production database (Render)
+    DATABASES = {
+        'default': dj_database_url.config(default=config('DATABASE_URL'))
+    }
 
 
 # Password validation
